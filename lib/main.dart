435a98ff183051932feaf048f9c3ad6fd10ea6e6
MANIKANDAN <mani.proff@gmail.com>
2022-09-26 22:42:01 +0530

    Add container card on widget.

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,90 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: AssetImage('images/mani.jpg'),
+              ),
+              Text(
+                'Manikandan T',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                ' SENIOR DATA SCIENTIST',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
               Container(
-                height: 100.0,
-                child: Text('container'),
-                color: Colors.red,
-              ),
-              SizedBox(
-                height: 20.0,
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ), //vertical will top and bottom & horizontal will be left and right
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 30.0,
+                      color: Colors.teal,
+                      // color: Colors.teal.shade600,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '+91 8870910142',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
               ),
               Container(
-                height: 100.0,
-                child: Text('container2'),
-                color: Colors.blue,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 100.0,
-                child: Text('container3'),
-                color: Colors.green,
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ), //vertical will top and bottom & horizontal will be left and right
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      size: 30.0,
+                      color: Colors.teal,
+                      // color: Colors.teal.shade600,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'mani.proff@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
