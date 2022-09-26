465d9283532c3c0474bb751ab01c8efe7a1091a3
MANIKANDAN <mani.proff@gmail.com>
2022-09-25 20:41:27 -0700

    fixed issues of compilesdkversion

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100.0,
+                child: Text('container'),
+                color: Colors.red,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                child: Text('container2'),
+                color: Colors.blue,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                child: Text('container3'),
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
